048f745656b6de7886ab6be35768cba2abf1ae17
James <jamesestrera82@gmail.com>
2025-08-31 19:59:20 +0800

    Completed runGame function

--- solution.js
+++ solution.js
@@ -5,19 +5,30 @@ import data from "./data.js";
 const word = data[Math.floor(Math.random() * data.length)];
 
 async function runGame () {
-    const answer =  await promp(`${word.question} `);
+    let attempts = word.word.length;
+    let progress = "";
+    
+    while (attempts > 0) {
+        const answer =  await promp(`${word.question} `);
 
-    if (answer.toLowerCase() !== word.word) {
-
-    } else {
-        console.log("Congratulations! You've guessed the word correctly.");
-        return;
+        if (answer.toLowerCase() === word.word) {
+            console.log("Congratulations! You've guessed the word correctly.");
+            return;
+        } else {
+            attempts--;
+            progress = "HANGMAN".slice(0, word.word.length - attempts);
+            
+            if (attempts === 0){
+                console.log(`failed to guessed the word. The correct answer is ${word.word}".`);
+                return;
+            } else if (attempts === 1) {
+                console.log(`Progress: ${progress}`);
+                console.log(`Hint: ${word.hint}`);
+            } else {
+                console.log(`Wrong! ${progress} (${attempts} attempts left)`);
+            }
+        }
     }
-
-
-    return runGame();
 }
-
-
 
 runGame();
